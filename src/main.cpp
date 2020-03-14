2280ea64ce4a6e549f54ad519139f562b954ae27
suupen <adelie064@gmail.com>
2020-03-14 20:31:43 +0900

    led3追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <mbed.h>
 DigitalOut myled(LED1);
 DigitalOut myled2(LED2);
+DigitalOut myled3(LED3);
+
 int main()
 {
 
@@ -10,10 +12,11 @@ int main()
     // put your main code here, to run repeatedly:
     myled = 1;
     myled2 = 0;
+    myled3 = 1;
     wait(0.5);
     myled = 0;
     myled2 = 1;
+    myled3 = 0;
     wait(1.2);
-    
-  }
+   }
 }
